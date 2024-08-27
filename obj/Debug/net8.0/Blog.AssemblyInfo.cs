437aca7024cf17456c5d190e7300667367c8b124
seif <seif@seif.com>
2024-08-27 18:50:51 +0300

    .

--- obj/Debug/net8.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net8.0/Blog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5b0109fba2870a7206fe4a6eecfa28c54dd1782")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1e52ef35fcd7af886299516fcf7b3a3b45b7793")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
